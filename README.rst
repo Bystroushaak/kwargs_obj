83a2739f9b567aeb791bbc5d51d73cd5addc1efa
Bystroushaak <bystrousak@kitakitsune.org>
2015-05-22 19:07:51 +0200

    Created using cookiecutter.

---
+++ README.rst
@@ -0,0 +1,18 @@
+kwargs_obj
+===============================
+
+.. image:: https://badge.fury.io/py/kwargs_obj.png
+    :target: http://badge.fury.io/py/kwargs_obj
+
+.. image:: https://pypip.in/d/kwargs_obj/badge.png
+        :target: https://pypi.python.org/pypi/kwargs_obj
+
+
+Class that maps kwargs to attributes.
+
+Documentation
+-------------
+
+Full module documentation and description can be found at Read the Docs:
+
+- http://kwargs_obj.readthedocs.org/
---
+++ tests/test_kwargs_obj.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import sys
+
+import pytest
+
+sys.path.append('src/')
+import kwargs_obj
+
+
+# Variables ===================================================================
+
+
+
+# Functions & classes =========================================================
+def test_kwargs_obj():
+    pass
